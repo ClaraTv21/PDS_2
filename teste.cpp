6d42709363a0bcf625c3f82bcfffb293a3901a29
ClaraTv21 <clara21ufmg@gmail.com>
2023-04-29 14:09:13 -0300

    Teste

---
+++ teste.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string palavra;
+
+    cout << "Digite uma palavra: ";
+    cin >> palavra;
+    cout << "A palavra que voce digitou foi:";
+
+    return 0;
+
+}
